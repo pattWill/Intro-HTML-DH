2a23d875cf9e2517e791e66053472a1be365fa57
pattWill <pattywilliams20@gmail.com>
2022-01-22 22:43:07 -0300

    usando app.get con req y res para definir en el navegador la ruta home con path.resolve y dirname

--- app.js
+++ app.js
@@ -9,3 +9,6 @@ app.use( express.static(publicPath))
 app.listen(3000, ()  => console.log('Levantando un servidor con Express'));
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
